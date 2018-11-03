fc35dbf0d50f938ee501d8f51cd20adb658f05af
coffee <1219774571@qq.com>
2018-11-04 05:31:07 +0800

    用于爬取百度图片，加入线程池

--- Test/baidu.py
+++ Test/baidu.py
@@ -1,7 +1,14 @@
+# 作者: coffee
+# 时间: 2018年11月04日05:28:23
+# 脚本: 爬取百度图片的爬虫
+# 版本: V1.1
+
 import getopt
 import hashlib
 import os
+import re
 import sys
+from multiprocessing.pool import Pool
 from urllib.parse import urlencode
 import requests
 from requests import RequestException
@@ -12,9 +19,11 @@ headers = {
     'Connection': 'keep-alive',
     'Referer': ''
     }
+args = {}
 
 
-def get_url_page(num, name):
+def get_url_page(num):
+    name = args['i']
     data = {
         'tn': 'resultjson_com',
         'ipn': 'rj',
@@ -33,7 +42,8 @@ def get_url_page(num, name):
     return 'http://image.baidu.com/search/acjson?' + urlencode(data)
 
 
-def get_one_page(url, name):
+def get_one_page(url):
+    name = args['i']
     index_url = {
         'tn': 'baiduimage',
         'ct': '201326592',
@@ -62,47 +72,57 @@ def parse_one_page(html):
     img = []
     for i in html['data']:
         if 'hoverURL' in i:
-            #if re.match('http(.*?)jpg', i['hoverURL'], re.S):
-            img.append(i['hoverURL'])
+            if re.match('http(.*?)jpg', i['hoverURL'], re.S):
+                img.append(i['hoverURL'])
     return img
 
 
-def download_img(url, path):
+def download_img(url):
     try:
         response = requests.get(url, stream=True, headers=headers)
     except RequestException:
         print("下载错误", response.status_code)
         return
     if response.status_code == 200:
-        save_image(response.content, url, path)
+        save_image(response.content, url)
 
 
-def save_image(img, url, path):
-    if not os.path.isdir(os.getcwd() + '/images'):
-        if path == os.getcwd() + '/images':
-            os.mkdir(path, 0o755)
+def parse_path():
+    if 'o' not in args.keys():
+        if not os.path.isdir(os.getcwd() + '/images'):
+            os.mkdir(os.getcwd() + '/images', 0o755)
+        return os.getcwd() + '/images'
+    else:
+        return args['o']
+
+
+def save_image(img, url):
+    path = parse_path()
     path = '{0}/{1}.{2}'. format(path, hashlib.md5(url.encode('utf-8')).hexdigest(), 'jpg')
     if not os.path.exists(path):
         with open(path, 'wb') as f:
             f.write(img)
             f.close()
+            print('此图片', url, '下载完成')
 
 
-def main(offset, name, path=os.getcwd()+"/images"):
-    url = get_url_page(offset, name)
+def main(offset):
+    url = get_url_page(offset)
     print("解析url为", url)
-    response = get_one_page(url, name)
+    response = get_one_page(url)
     url = parse_one_page(response)
-    print("获取成功，进入下载")
     for image in url:
         if image:
-            print(image)
-            download_img(image, path)
+            download_img(image)
 
 
 def parse_args():
+    if len(sys.argv) < 2:
+        print("-n number")
+        print("-i 爬取图片名字")
+        print("-o 输出目录，无则下载到当前目录images")
+        sys.exit(2)
     opts, arg = getopt.getopt(sys.argv[1:], "n:i:o:")
-    args = {}
     for option in opts:
         if option[0] == '-n':
             args['n'] = option[1]
@@ -112,25 +132,16 @@ def parse_args():
             args['o'] = option[1]
         else:
             print("错误参数", option[0])
-    return args
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("-n number")
-        print("-i 爬取图片名字")
-        print("-o 输出目录，无则下载到当前目录images")
-        sys.exit(2)
-    args = parse_args()
     if 'n' not in args.keys() or 'i' not in args.keys():
         print("数值和图片名是必须的")
         sys.exit(3)
 
-    s = [30*i for i in range(int(args['n']))]
-    for num in s:
-        print("当前第%d轮" % int(num / 30))
-        if 'o' in args.keys():
-            main(num, args['i'], args['o'])
-        else:
-            main(num, args['i'])
 
+if __name__ == '__main__':
+    parse_args()
+    s = [30*i for i in range(int(args['n']))]
+    pool = Pool()
+    if 'o' in args.keys():
+        pool.map(main, s)
+    else:
+        pool.map(main, s)
